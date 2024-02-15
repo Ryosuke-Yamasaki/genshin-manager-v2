bf799365a0346749e476c10535de9c3ff6cba8e1
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-02-15 18:20:31 +0900

    create interface file

--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -1,12 +1,34 @@
-export interface ArtifactTypes {
+interface ArtifactTypes {
   id: number;
   japanese: string;
   english: string;
 }
 
-export interface ArtifactMainStats {
+interface ArtifactMainStats {
   statId: number;
   star4: number;
   star5: number;
   Stats: { id: number; japanese: string; english: string; text: string };
 }
+
+interface ArtifactIcons {
+  artifactId: number;
+  url: string;
+}
+
+interface SelectArtifactTypeProps {
+  typeId: number;
+}
+
+interface SelectArtifactMainStatProps {
+  typeId: string;
+  form: any;
+}
+
+export type {
+  ArtifactTypes,
+  ArtifactMainStats,
+  ArtifactIcons,
+  SelectArtifactTypeProps,
+  SelectArtifactMainStatProps,
+};
