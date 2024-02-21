b3ed080917c3e16b5c54556287472de45d57e7d3
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-02-21 10:53:29 +0900

    スコア計算の追加

--- src/actions/postArtifacters.ts
+++ src/actions/postArtifacters.ts
@@ -12,6 +12,17 @@ export const PostArtifacters = async (
 ) => {
   const { userId, artifactId, typeId, setId, mainStatId, subOptions } = value;
 
+  const scoreMultipliers: { [K: string]: number } = {
+    "600": 2,
+    "700": 1,
+  };
+
+  const score = subOptions
+    .filter((data) => Object.keys(scoreMultipliers).includes(data.statId))
+    .map((data) => Number(data.value) * scoreMultipliers[data.statId])
+    .reduce((a, b) => a + b)
+    .toFixed(3);
+
   subOptions.map((data) => {
     if (!IntStatId(data.statId.toString()))
       data.value = (Number(data.value) / 100).toFixed(3).toString();
@@ -21,6 +32,7 @@ export const PostArtifacters = async (
   const supabase = createClient(cookieStore);
   const { error } = await supabase.from("Artifacters").insert({
     userId,
+    score,
     artifactId,
     typeId,
     setId,
