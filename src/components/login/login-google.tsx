2314729993b0537e5632830e2c8241b9ef780e3d
山﨑亮輔 <ryosuke@yamazakiryousukes-MacBook-Air.local>
2024-02-29 10:07:47 +0900

    Google認証のところを変更

--- src/components/login/login-google.tsx
+++ src/components/login/login-google.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { createBrowserClient } from "@supabase/ssr";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../ui/button";
+import { createClient } from "@/lib/supabase/client";
 
 const LoginWithGoogleButton = () => {
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabase = createClient();
+
   const LoginWithGoogle = () => {
     supabase.auth.signInWithOAuth({
       provider: "google",
